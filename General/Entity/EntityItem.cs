c34abafe7e40744b3485cef6bae9f3b951658a00
me <me@WURK>
2016-11-07 22:19:18 -0700

    entity

---
+++ General/Entity/EntityItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace General.Entity
+{
+    public class EntityItem
+    {
+        public int ParentId { get; set; }
+        public int ItemId { get; set; }
+
+        public string Name{ get; set; }
+
+    }
+}
---
+++ General/Entity/EntityValue.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace General.Entity
+{
+    public class EntityValue
+    {
+        public int ItemId { get; set; }
+        public int Text{ get; set; }
+        public DateTime Time{ get; set; }
+        public decimal Number{ get; set; }
+
+    }
+}
